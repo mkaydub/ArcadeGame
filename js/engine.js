9106569ab5e75591bfe1dad1a792dd0d89eee78a
Mellissa-Key-West <art@mkaydub.com>
2018-08-15 15:19:19 -0600

    Added Char-cat-girl to resources for load.

--- js/engine.js
+++ js/engine.js
@@ -174,7 +174,8 @@ var Engine = (function(global) {
     'images/water-block.png',
     'images/grass-block.png',
     'images/enemy-bug.png',
-    'images/char-boy.png'
+    'images/char-boy.png',
+    'images/char-cat-girl.png'
   ]);
   Resources.onReady(init);
   /* Assign the canvas' context object to the global variable (the window
