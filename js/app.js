230613f570e1a6cc4f800536a100ab3417a03451
Mellissa-Key-West <art@mkaydub.com>
2018-08-15 15:20:29 -0600

    CHange from var to const

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ class Player {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var player = new Player();
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
