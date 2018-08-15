7ddd0de47d28e9269fea68110530061d38c00a45
Mellissa-Key-West <art@mkaydub.com>
2018-08-15 15:22:16 -0600

    Uncommenting player.render so that the player renders.

--- js/engine.js
+++ js/engine.js
@@ -154,7 +154,7 @@ var Engine = (function(global) {
     //    enemy.render();
     //  });
 
-    //  player.render();
+    player.render();
   }
 
   /* This function does nothing but it could have been a good place to
