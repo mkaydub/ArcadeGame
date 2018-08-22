97774544091b4b4d292ad7fecc4eec3363bb834c
Mellissa-Key-West <art@mkaydub.com>
2018-08-22 10:42:56 -0600

    Changed playing character from char-horn-girl.png to ghost-player.png

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 class Player {
   constructor(x, y) {
-    this.sprite = 'images/char-horn-girl.png';
+    this.sprite = 'images/ghost-player.png';
     this.x = x;
     this.y = y;
     this.horiz = 101;
