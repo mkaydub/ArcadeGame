40d133efe3915ac4386337748acbc7a3f0a59244
Mellissa-Key-West <art@mkaydub.com>
2018-08-22 10:51:44 -0600

    Removing empty space.

--- js/app.js
+++ js/app.js
@@ -222,7 +222,6 @@ const enemy4 = new Enemy(-30, 0);
 const allEnemies = [];
 allEnemies.push(enemy1, enemy2, enemy3, enemy4);
 
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
